4232815bf2790c7beff74098b956d8708273a8fa
paulMolano <paulmolanoo@gmail.com>
2021-12-21 16:30:06 +0100

    Login v1.0

--- cliente/src/components/auth/Login.js
+++ cliente/src/components/auth/Login.js
@@ -1,7 +1,45 @@
 import React from "react";
 
 const Login = () => {
-  return <h1>Desde Login</h1>;
+  const onChange = () => {};
+
+  return (
+    <div className="form-usuario">
+      <div className="contenedor-form sombra-dark">
+        <h1>Iniciar Sesión</h1>
+
+        <form>
+          <div className="campo-form">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Email"
+              onChange={onChange}
+            />
+          </div>
+          <div className="campo-form">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Password"
+              onChange={onChange}
+            />
+          </div>
+          <div className="campo-form">
+            <input
+              type="submit"
+              className="btn btn-primario btn-block"
+              value="Iniciar Sesión"
+            />
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
