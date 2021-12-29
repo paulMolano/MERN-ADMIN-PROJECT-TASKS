799dbee5a1d0b5152973a7093606fd8e43cd1467
paulMolano <paulmolanoo@gmail.com>
2021-12-29 10:08:05 +0100

    validacion usuarios con express-validator

--- servidor/controllers/usuarioController.js
+++ servidor/controllers/usuarioController.js
@@ -1,3 +1,37 @@
-exports.crearUsuario = (req, res) => {
-  console.log(req.body);
+const Usuario = require("../models/Usuario");
+const bcryptjs = require("bcryptjs");
+const { validationResult } = require("express-validator");
+
+exports.crearUsuario = async (req, res) => {
+  //revisar si hay errores
+  const errores = validationResult(req);
+  if (!errores.isEmpty()) {
+    return res.status(400).json({ errores: errores.array() });
+  }
+
+  //extraer email y password
+  const { email, password } = req.body;
+
+  try {
+    //revisar que el usuario registrado sea unico
+    let usuario = await Usuario.findOne({ email });
+
+    if (usuario) {
+      return res.status(400).json({ msg: "El usuario ya existe" });
+    }
+    //crear nuevo usuario
+    usuario = new Usuario(req.body);
+
+    //Hashear el password
+    const salt = await bcryptjs.genSalt(10);
+    usuario.password = await bcryptjs.hash(password, salt);
+
+    //guardar usuario
+    await usuario.save();
+
+    res.json({ msg: "Usuario creado correctamente" });
+  } catch (error) {
+    console.log(error);
+    res.status(400).send("Error al instertar el nuevo Usuario");
+  }
 };
