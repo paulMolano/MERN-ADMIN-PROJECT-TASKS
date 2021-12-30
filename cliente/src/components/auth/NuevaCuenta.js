5ad5918d4447d7c3c768069f7e5f4549cd4f83fb
paulMolano <paulmolanoo@gmail.com>
2021-12-30 12:10:13 +0100

    Validacion de formularios

--- cliente/src/components/auth/NuevaCuenta.js
+++ cliente/src/components/auth/NuevaCuenta.js
@@ -1,7 +1,12 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import { Link } from "react-router-dom";
+import alertaContext from "../../context/alertas/alertaContext";
 
 const NuevaCuenta = () => {
+  //Extraer valores del context
+  const alertasContext = useContext(alertaContext);
+  const { alerta, mostrarAlerta } = alertasContext;
+
   //State para iniciar sesión
   const [nuevoUsuario, guardarnuevoUsuario] = useState({
     nombre: "",
@@ -27,18 +32,47 @@ const NuevaCuenta = () => {
     console.log(nuevoUsuario);
 
     //Validar que no haya campos vacios
+    if (
+      nombre.trim() === "" ||
+      email.trim() === "" ||
+      password.trim() === "" ||
+      confirmar.trim() === ""
+    ) {
+      mostrarAlerta("Todos los campos son obligatorios", "alerta-error");
+      return;
+    }
 
     //Password minimo de 6 caracteres
+    if (password.trim().length < 6) {
+      mostrarAlerta(
+        "La contraseña debe de tener mínimo 6 caracteres",
+        "alerta-error"
+      );
+      return;
+    }
 
     //Ambos password seán iguales
+    if (password.trim() !== confirmar.trim()) {
+      mostrarAlerta("Ambas contraseñas deben de ser iguales", "alerta-error");
+      return;
+    }
 
     //Pasarlo al action
 
     //Limpiar los inputs
+    guardarnuevoUsuario({
+      nombre: "",
+      email: "",
+      password: "",
+      confirmar: "",
+    });
   };
 
   return (
     <div className="form-usuario">
+      {alerta ? (
+        <div className={`alerta ${alerta.categoria}`}>{alerta.msg}</div>
+      ) : null}
       <div className="contenedor-form sombra-dark">
         <h1>Crear Nueva Cuenta</h1>
 
